b4ff95641393bcd5745aa3b32ba26009aef02654
pkeinde@gmail.com <pkeinde@gmail.com>
2025-01-09 20:08:25 +0000

    version 1.1.4.a.d

--- service.php
+++ service.php
@@ -34,12 +34,14 @@
     </nav>
 </header>
 <section class="main">
-    <img src="https://static.wixstatic.com/media/ea71bb_370144e721bc43c289bfb35fed3376d1~mv2_d_4500_2124_s_2.jpg/v1/fill/w_1632,h_534,al_b,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/ea71bb_370144e721bc43c289bfb35fed3376d1~mv2_d_4500_2124_s_2.jpg" width="85%" style="box-shadow: 0 4px 8px rgb(0 0 0 / 64%);">
-    <div class="main-title">
-        <h1>Services</h1>
-        <div>
-            <div></div>
-            <p>Paragraphe. Cliquez ici pour ajouter votre propre texte. Cliquez sur « Modifier Texte » ou double-cliquez ici pour ajouter votre contenu et personnaliser les polices. Relatez ici votre parcours et présentez votre activité à vos visiteurs.</p>
+    <img src="Nouveau-dossier\Nouveau dossier\HB.png" width="85%" style="box-shadow: 0 4px 8px rgb(0 0 0 / 30%);">
+    <div class="main-container">
+        <div class="main-title">
+            <h1>Services</h1>
+            <div>
+                <div></div>
+                <p>Paragraphe. Cliquez ici pour ajouter votre propre texte. Cliquez sur « Modifier Texte » ou double-cliquez ici pour ajouter votre contenu et personnaliser les polices. Relatez ici votre parcours et présentez votre activité à vos visiteurs.</p>
+            </div>
         </div>
     </div>
 </section>
@@ -49,17 +51,44 @@
         <h1>Nos services</h1>
         <p>Paragraphe. Cliquez ici pour ajouter votre propre texte. C'est facile ! Cliquez sur « Modifier Texte » ou double-cliquez ici.</p>
     </div>
-    <div class="service-text">
-    jdfcytfg
-    </div>
 </section>
-       <!-- 
-            <div class="marketing">
-            <h2>Design Graphique</h2>
+
+<section class="service-text">
+       <!-- <div class="marketing"><h2></h2></div> -->
+    <div>
+    <div class="s-container">
+            <div class="s-item">
+                <div>
+                <img src="https://thumbs.dreamstime.com/b/stylo-plume-avec-l-illustration-d-%C3%A9l%C3%A9ment-de-logo-ic%C3%B4ne-du-vecteur-encre-isol%C3%A9e-sur-le-fond-noir-feuille-isol%C3%A9-classique-185210448.jpg" width="100" style="border-radius: 50%;" alt="Icône stylisé de plume">
+                <h2>Design Graphique</h2>
+                <p>Une bonne communication passe par des insignes explicites  répondant aux exigences des codes couleurs. Nous extérioriserons vos goûts à travers une large gamme de concepte  branding 2D et 3D avec une impression numérique de qualité... <a href="">Voir plus</a></p>
+                </div>
+            </div>
+            <div class="s-item">
+                <div>
+                    <img src="https://cbx-prod.b-cdn.net/COLOURBOX18689421.jpg?width=800&height=800&quality=70" width="100" style="border-radius: 50%;" alt="Icône représentant un écran de développement web">
+                    <h2>Développement web/mobile</h2>
+                    <p>Nous matérialiserons vos idées avec une conception de software adaptée à vos besoins. Des sites Web sur mesure, d'applications mobiles Android / ios et web, sites e-commerce, vitrine et learning. Sans oublier des solutions synchroniser avec l'IA...<a href="">Voir plus</a></p>
+                </div>
+            </div>
            
-        </div>
-        -->
-    <div></div>
+            <div class="s-item">
+                <div>
+                    <img src="https://cdn.icon-icons.com/icons2/1233/PNG/512/1492718748-share_83604.png" width="100" style="border-radius: 50%;" alt="Icône marketing digital">
+                    <h2>Marketing Digital</h2>
+                    <p>Dans un monde dominé par les médis sociaux, le digital marketing reste un allié incontournable. Que vous voyez professionnels ou structure PME, vous avez besoin des techniques digitaux pour bien faire valoir vos produits et services...<a href="">Voir plus</a></p>
+                </div>
+            </div>
+            
+            <div class="s-item">
+                <div>
+                    <img src="Nouveau-dossier\reseau.png" width="100" style="border-radius: 50%;" alt="Icône système et réseau">
+                    <h2>Système et Réseau</h2>
+                    <p>Une équipe expérimentée va prendre en charge votre structures informatique avec un dépannage préventif et curatif. Nous offrons également une installation de système réseau adaptée à votre parc IT ainsi que toute installation de vidéo surveillance et de support Wifi...<a href="">Voir plus</a></p>
+                </div>
+            </div>
+    </div>
+    </section>
 <script src="script.js"></script> <!-- Lien vers le fichier JS -->
 <footer></footer>
 </body>
